5c4c3c0e8c1c4a3274e99c47363c81d755db6019
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2025-11-02 11:30:47 -0600

    add week 2 files

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,25 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Kaitlyn Kelly
+ * Date: 11/2/25
+ * File Name: index.js
  * Description:
 */
 
 // TODO: Import your module using require
+const {createRecipe, setTimer, quit} = require("./recipes");
 
 // TODO: Implement your CLI program here
+const ingredients = [
+    "ingredient1",
+    "ingredient2"
+];
+
+const minutes = 15;
+
+function main() {
+    console.log(createRecipe(ingredients));
+    console.log(setTimer(minutes));
+    console.log(quit());
+}
+
+main();
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,31 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Kaitlyn Kelly
+ * Date: 11/2/25
+ * File Name: recipes.js
  * Description:
 */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
   // TODO: Implement this function
+  return "Recipe created with ingredients: " + ingredients[0] + ", " + ingredients[1];
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
   // TODO: Implement this function
+  return "Timer set for " + minutes + " minutes";
 }
 
 // Define the quit function
 function quit() {
   // TODO: Implement this function
+  return "Program exited";
 }
 
 // TODO: Export the functions
+module.exports = {
+  createRecipe: createRecipe,
+  setTimer: setTimer,
+  quit: quit,
+};
