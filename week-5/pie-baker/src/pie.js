a71fca818ec6330cef63a8e05f4fd3ad04d5dc5d
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2025-11-22 12:39:09 -0600

    add week 5 files

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,32 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Kaitlyn Kelly 
+ * Date: 11/22/25
+ * File Name: pie.js
  * Description:
  */
 "use strict";
 
+const essentialIngredients = ["flour", "sugar", "butter"];
+
 function bakePie(pieType, ingredients) {
   // Your code here
+
+  // iterate through essentialIngredients to see if each ingredient was passed to the function
+  for (let i = 0; i < essentialIngredients.length; i++) {
+    const currentIngredient = essentialIngredients[i];
+
+    // if an essentialIngredient was not passed to the function, stop the loop then print and return an error message
+    if (!ingredients.includes(currentIngredient)) {
+      let failedMessage = `${pieType} was not baked due to missing ${currentIngredient}.`;
+      console.error(failedMessage); // print error message
+      process.exit(1) // exit
+      return failedMessage; // return error message
+    }
+  }
+
+  let successMessage = `${pieType} was successfully baked. Let's eat!`;
+  console.log(successMessage); // print success message
+  return successMessage; // return success message
 }
 
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,7 +1,7 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Kaitlyn Kelly
+ * Date:11/22/25
+ * File Name: pie.spec.js
  * Description:
  */
 
@@ -10,3 +10,18 @@
 const { bakePie } = require("../src/pie");
 
 // Your tests here
+
+test("bake a pumpkin pie successfully with all essential ingredients", () => {
+    const result = bakePie("Pumpkin Pie", ["flour", "pumpkin", "sugar", "brown sugar", "butter", "sweetened condensed milk"])
+    expect(result).toBe("Pumpkin Pie was successfully baked. Let's eat!");
+});
+
+test("bake a cherry pie successfully with all essential ingredients", () => {
+    const result = bakePie("Cherry Pie", ["cherries", "sugar", "butter", "flour"])
+    expect(result).toBe("Cherry Pie was not baked due to missing flour.");
+});
+
+test("unsuccessfully bake a pecan pie because sugar is missing", () => {
+    const result = bakePie("Pecan Pie", ["pecans", "brown sugar", "flour", "butter"])
+    expect(result).toBe("Pecan Pie was not baked due to missing sugar.");
+});
