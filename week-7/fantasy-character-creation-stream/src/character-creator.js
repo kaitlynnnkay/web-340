c903ab21bc8c43d3f28b873e4321698d35aa24c1
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2025-12-07 14:57:28 -0600

    add week 7 files

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,40 @@
+/*
+Name: Kaitlyn Kelly
+Date: 12/4/25
+File: character-creator.js
+*/
+
 const { Duplex } = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
     // TODO: Initialize your class here
+    this.characters = []; // array to store character data
   }
+
 
   _write(chunk, encoding, callback) {
     // TODO: Implement your _write method here
+    try {
+      const characterData = JSON.parse(chunk.toString()); // convert chunk into a string & then parse it as JSON
+      this.characters.push(characterData); // store parsed data in the characters array
+      callback();
+    } catch (err) {
+      callback(err);
+    }
   }
 
   _read(size) {
     // TODO: Implement your _read method here
+    if (this.characters.length === 0) { // push null when no characters left
+      this.push(null);
+      return;
+    }
+
+    const char = this.characters.shift();
+    const charDescription = `Created character is a ${char.gender} ${char.class}. Character fun fact: ${char.funFact}.\n`; // formatted output from parsed data
+    this.push(charDescription); // push the formatted output out of the stream
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,3 +1,9 @@
+/*
+Name: Kaitlyn Kelly
+Date: 12/6/25
+File: character-creator.spec.js
+*/
+
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
@@ -9,13 +15,47 @@ describe('CharacterCreator', () => {
 
   test("should process data correctly when written to", (done) => {
     // TODO: Write your test here
+    characterCreator.write(JSON.stringify({
+      class: "Warrior",
+      gender: "Female",
+      funFact: "Preferred weapon is an axe."
+    }));
+
+    characterCreator.on('data', (chunk) => {
+      const output = chunk.toString();
+      expect(output).toContain("Warrior");
+      expect(output).toContain("Female");
+      expect(output).toContain("Preferred weapon is an axe.");
+      done();
+    });
+
+    characterCreator.read();
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
     // TODO: Write your test here
+    characterCreator.on('error', (err) => {
+      expect(err).toBeTruthy();
+      done();
+  });
+
+  characterCreator.write("");
   });
 
   test("should transform data correctly when written to", (done) => {
     // TODO: Write your test here
+    const input = {
+      class: "Warrior",
+      gender: "Female",
+      funFact: "Preferred weapon is an axe"
+    };
+
+    characterCreator.write(JSON.stringify(input));
+
+    characterCreator.on('data', (chunk) => {
+      const output = chunk.toString();
+      expect(output).toBe("Created character is a Female Warrior. Character fun fact: Preferred weapon is an axe.\n");
+      done();
+    });
   });
 });
