39b5ca5aef26cde2fcb6eba63e72ffc295e03b31
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2025-12-21 13:50:56 -0600

    add week 9 files

--- week-9/fantasy-game-characters/src/game-characters-data.js
+++ week-9/fantasy-game-characters/src/game-characters-data.js
@@ -1,1 +1,10 @@
 // TODO: Implement this script
+
+"use strict";
+
+const gameCharacters = [
+    { class: "Warrior", gender: "Female", fact: "Preferred weapon is an axe." },
+    { class: "Mage", gender: "Female", fact: "Ironically, hates magic." },
+];
+
+console.log(JSON.stringify(gameCharacters));
--- week-9/fantasy-game-characters/test/game-characters.spec.js
+++ week-9/fantasy-game-characters/test/game-characters.spec.js
@@ -10,13 +10,33 @@ describe("GameCharacters", () => {
 
   test("should return game characters data", (done) => {
     // TODO: Implement this test
+    gameCharacters.getCharacters((data, error) => {
+      expect(error).toBeNull();
+      expect(data).toEqual([
+        { class: "Warrior", gender: "Female", fact: "Preferred weapon is an axe." },
+        { class: "Mage", gender: "Female", fact: "Ironically, hates magic." },
+      ]);
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script is not found", (done) => {
     // TODO: Implement this test
+    const gameCharacters = new GameCharacters("nonexistent-script.js");
+    gameCharacters.getCharacters((data, error) => {
+      expect(data).toBeNull();
+      expect(error).not.toBeNull();
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script fails", (done) => {
     // TODO: Implement this test
+    const gameCharacters = new GameCharacters("failing-script.js");
+    gameCharacters.getCharacters((data, error) => {
+      expect(data).toBeNull();
+      expect(error).not.toBeNull();
+      done();
+    });
   });
 });
